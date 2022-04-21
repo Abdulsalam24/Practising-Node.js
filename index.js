77c006cc9daa565a649b438e3aea6876e782697f
Abdulsalam24 <abdulsalammohammed586686@gmail.com>
2022-04-21 14:40:54 -0700

    first practice section is over

---
+++ index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+const os = require('os')
+// const url = require('url')
+
+const myUrl = new URL ('http://mysite.com/hello.html?id=100&status=100')
+
+console.log(myUrl.searchParams)
+myUrl.searchParams.append('htis' , 'abc')
+
+myUrl.searchParams.forEach((value , name) => console.log(`this is the ${value} and the ${name}`))
