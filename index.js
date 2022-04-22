c0957daf62e1cf3a6d733ff95322883e8aa3cd8a
Abdulsalam24 <abdulsalammohammed586686@gmail.com>
2022-04-22 13:51:30 -0700

    install express to my nod

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const os = require('os')
+const express = require('express')
 // const url = require('url')
 
 const myUrl = new URL ('http://mysite.com/hello.html?id=100&status=100')
