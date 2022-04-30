0b9e625f1c4745cd25bae67f3caee79819af1fdf
Abdulsalam24 <abdulsalammohammed586686@gmail.com>
2022-04-30 12:53:07 -0700

    node

--- index.js
+++ index.js
@@ -17,19 +17,7 @@ const server = http.createServer((req, res) => {
     res.end(JSON.stringify(user));
     res.writeHead(200, { "content-Type": "application.js" });
   }
-});
+}); 
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`server running...${PORT}`));
-
-
-//   if (req.url === "/") {
-//     fs.readFile(
-//       path.join(__dirname, "public", "index.html"),
-//       (err, content) => {
-//         if (err) throw err;
-//         res.writeHead(200, { "content-Type": "text/html" });
-//         res.end(content);
-//       }
-//     );
-//   }
+server.listen(PORT, () => console.log(`server running on  port${PORT}`));
--- logger.js
+++ logger.js
@@ -1,17 +1,15 @@
 
 
 
-const Logger = require('./logger')
-const EventEmitter = require('events')
+// const Logger = require('./logger')
+// const EventEmitter = require('events')
 
-const logger = new Logger()
+// const logger = new Logger()
 
-logger.on('message' , (data) => console.log('called listener :' , data))
-
-
-logger.log()
+// logger.on('message' , (data) => console.log('called listener :' , data))
 
 
+// logger.log()
 
 
 
@@ -41,22 +39,24 @@ logger.log()
 
 
 
-const EventEmitter = require('events')
-const uuid = require('uuid')
-class Logger extends EventEmitter{
-     log(msg){
-        this.emit('message' , {id : uuid.v4() , msg})
-     }
-}
 
-class MyEmitter extends EventEmitter { }
 
-const myEmitter = new MyEmitter();
+// const EventEmitter = require('events')
+// const uuid = require('uuid')
+// class Logger extends EventEmitter{
+//      log(msg){
+//         this.emit('message' , {id : uuid.v4() , msg})
+//      }
+// }
 
-myEmitter.on('event' , () => console.log('fired'))
+// class MyEmitter extends EventEmitter { }
 
-myEmitter.emit('event')
-myEmitter.emit('event')
-myEmitter.emit('event')
-myEmitter.emit('event')
-myEmitter.emit('event')
+// const myEmitter = new MyEmitter();
+
+// myEmitter.on('event' , () => console.log('fired'))
+
+// myEmitter.emit('event')
+// myEmitter.emit('event')
+// myEmitter.emit('event')
+// myEmitter.emit('event')
+// myEmitter.emit('event')
