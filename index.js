2c6f1a35efdb4ceb19abae1a97a3a85f22df818a
Abdulsalam24 <abdulsalammohammed586686@gmail.com>
2022-05-11 12:43:57 -0700

    replace static html with ejs view engine

--- index.js
+++ index.js
@@ -2,26 +2,46 @@ const express = require('express')
 
 const app = express()
 
+app.set('view engine' , 'ejs')
+
 app.listen(3000)
 
-
 app.get('/' , (req , res) => {
-    res.sendFile('./public/index.html' , {root : __dirname})
+    const blogs =[
+        {title : 'chicken' , author : 'samuel'},
+        {title : 'turkey' , author : 'emma'},
+        {title : 'cow' , author : 'ejiro'}
+    ]
+    res.render('index' , {blog : 'this is cool' , blogs});
 })
 
 app.get('/about' , (req , res) => {
-    res.sendFile('./public/about.html' , {root : __dirname})
-})
-app.get('/about-us' , (req , res) => {
-    res.redirect('/about')
+    res.render('about');
 })
 
-
-
+app.get('/create/blog' , (req , res) => {
+    res.render('blog');
+})
 
 app.use((req , res) => {
-    res.sendFile('./public/404.html' , {root : __dirname})
+    res.status(404).render('404');
 })
+
+
+
+// app.get('/about' , (req , res) => {
+//     res.sendFile('./public/about.html' , {root : __dirname})
+// })
+// app.get('/about-us' , (req , res) => {
+//     res.redirect('/about')
+// })
+
+
+
+
+// app.use((req , res) => {
+//     res.status(404).sendFile('./public/404.html' , {root : __dirname})
+// })
 
 
 
